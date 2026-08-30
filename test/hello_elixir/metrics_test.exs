fb9a2cb445d703622d5fb8f75910b0cf8833cdec
Brandon Cantrell <bcantr3ll@gmail.com>
2026-08-30 14:40:19 -0500

    style: complete failure lab formatting

--- test/hello_elixir/metrics_test.exs
+++ test/hello_elixir/metrics_test.exs
@@ -6,6 +6,7 @@ defmodule HelloElixir.MetricsTest do
     output = HelloElixir.Metrics.prometheus()
 
     assert output =~ "hello_api_http_requests_total{route=\"/work\",status=\"200\"} 1"
+
     assert output =~
              "hello_api_http_request_duration_seconds_bucket{route=\"/work\",le=\"0.025\"} 1"
 
