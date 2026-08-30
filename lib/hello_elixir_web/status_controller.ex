056448fa9a6944f1ca9560d071cc86665f6a7972
Brandon Cantrell <bcantr3ll@gmail.com>
2026-08-29 22:51:36 -0500

    feat: expose GitOps delivery metadata (#6)

--- lib/hello_elixir_web/status_controller.ex
+++ lib/hello_elixir_web/status_controller.ex
@@ -3,6 +3,7 @@ defmodule HelloElixirWeb.StatusController do
 
   def show(conn, _params) do
     json(conn, %{
+      delivery: "gitops",
       environment: Application.fetch_env!(:hello_elixir, :app_environment),
       status: "ok",
       version: Application.fetch_env!(:hello_elixir, :app_version)
